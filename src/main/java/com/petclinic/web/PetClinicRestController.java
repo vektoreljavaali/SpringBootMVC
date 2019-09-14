95719823f04502473034e140559ead63d847b536
Muham <Muham@192.168.1.21>
2019-09-15 01:17:04 +0300

    RestApi Denemeleri

--- src/main/java/com/petclinic/web/PetClinicRestController.java
+++ src/main/java/com/petclinic/web/PetClinicRestController.java
@@ -4,8 +4,11 @@ package com.petclinic.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +19,7 @@ import com.petclinic.model.Owner;
 
 import com.petclinic.service.PerClinicService;
 
+@CrossOrigin()
 @RestController
 @RequestMapping("/rest")
 public class PetClinicRestController {
@@ -42,6 +46,12 @@ public class PetClinicRestController {
 		
 		
 		return "OK";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST,value = "/ekleowner",consumes = "application/json")
+	public void ekle(@RequestBody Owner owner) {
+		System.out.println("Buraya Geldiler");
+		perClinicService.crateOwner(owner);		
 	}
 	
 	@RequestMapping(method = RequestMethod.GET,value = "/owner")
